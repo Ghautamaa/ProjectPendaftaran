9ba9fda232915be65cc0d40b5c5ea0d9419d0120
Demonic <134345277+IKomangYoga@users.noreply.github.com>
2023-06-06 16:26:17 +0800

    home

---
+++ resource/view/index.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+ 
+    <!-- Nav -->
+    <nav class="navbar navbar-light fixed-top" style="background-color: rgba(0, 0, 0, 0.95);">
+  <a class="navbar-brand" href="#">
+    <img src="https://stickershop.line-scdn.net/stickershop/v1/product/9738038/LINEStorePC/main.png" width="30" height="30" class="d-inline-block align-top" alt="">
+     Bootstrap
+  </a>
+  <a class="navbar-brand">Navbar</a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
+    <!-- End Nav -->
+
+<!-- Body -->
+<br>
+<br>
+<br>
+
+<div class="welcome" style=" padding-top: 10px; padding-left: 59px; padding-right: 50px; padding-bottom: 20px;">
+<h7>#Welcome To Zoofari</h7><br>
+<h1 style="display: inline;">Beberapa </h1><h1 style="color: gold; display: inline;">Keuntungan </h1> <h1 style="display: inline;">yang bisa kamu dapatkan : </h1>
+<br>
+<br>
+<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, nihil modi fuga porro nam id quaerat sint, aliquid labore ipsam unde omnis! Quisquam et, cum provident aut impedit inventore voluptas.</h4>
+<h4>
+    <li>Ayam Goreng</li>
+    <li>Sate Babi</li>
+</h4>
+</div>
+
+<div class="row row-cols-1 row-cols-md-3" style=" padding-top: 10px; padding-left: 50px; padding-right: 50px; padding-bottom: 20px;">
+        <!-- row-cols-md-4 buat ngatur besar kecilnya semua card -->
+
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://hips.hearstapps.com/hmg-prod/images/rays-of-sunlight-into-the-underwater-cave-royalty-free-image-1665512420.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <center>
+        <a href="game.php" style="color:black;">
+        <h5 class="card-title">Perlombaan</h5>
+        <p class="card-text">E-Sport</p>
+        </a>
+        </center>
+      </div>
+    </div>
+  </div>
+
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://static.theceomagazine.net/wp-content/uploads/2018/08/16090106/Deep-sea-tourism_Sam-McKeith.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <center>
+        <a href="game.php" style="color:black;">
+        <h5 class="card-title">Perlombaan E-Sport</h5>
+        <p class="card-text">Kuota : 50 orang</p>
+        </a>
+        </center>
+      </div>
+    </div>
+  </div>
+
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://hips.hearstapps.com/hmg-prod/images/rays-of-sunlight-into-the-underwater-cave-royalty-free-image-1665512420.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+      <center>
+        <h5 class="card-title">Perlombaan E-Sport</h5>
+        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p> -->
+        </center>
+      </div>
+    </div>
+  </div>
+
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://static.theceomagazine.net/wp-content/uploads/2018/08/16090106/Deep-sea-tourism_Sam-McKeith.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <center>
+        <h5 class="card-title">E-Sport</h5>
+        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
+        </center>  
+    </div>
+    </div>
+  </div>
+
+  <div class="col mb-4">
+    <div class="card">
+      <img src="https://hips.hearstapps.com/hmg-prod/images/rays-of-sunlight-into-the-underwater-cave-royalty-free-image-1665512420.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+<!-- End Body -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+
+  </body>
+</html>
