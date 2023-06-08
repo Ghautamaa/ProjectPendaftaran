0ac7cccce1fee4ef80bbe32772741e82a7394b89
gunturdwiap <kadekguntur13@gmail.com>
2023-06-08 16:21:22 +0800

    Menambah file form lomba tim

---
+++ resource/view/formLombaTim.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Login</title>
+</head>
+
+<body>
+<div class="container-fluid bg-primary">
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link text-white active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link text-white disabled" href="#">Disabled</a>
+        </li>
+    </ul>
+    <nav class="navbar navbar-light bg-primary justify-content-between">
+                <!-- Image and text -->
+        <nav class="navbar navbar-light bg-primary">
+        <a class="navbar-brand" href="#">
+            <img src="https://pnb.ac.id/img/logo-pnb.3aae610b.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Lomba coy
+        </a>
+        </nav>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </nav>
+</div>
+
+
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-6">
+      <form>
+            <div class="form-group">
+                <label for="namaTim">Nama Tim</label>
+                <input type="text" class="form-control" id="namaTim" placeholder="Nama Tim">
+            </div>
+            <div class="form-group">
+                <label for="noHP">No HP yang bisa dihubungi</label>
+                <input type="text" class="form-control" id="noHP" placeholder="No HP">
+            </div>
+            <div class="form-group">
+                <label for="namaTim">Nama Anggota Tim</label>
+                <input type="text" class="form-control" id="namaTim" placeholder="Nama Ketua">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="namaTim" placeholder="Nama Anggota">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="namaTim" placeholder="Nama Anggota">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="namaTim" placeholder="Nama Anggota">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="namaTim" placeholder="Nama Anggota">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="namaTim" placeholder="Nama Cadangan">
+            </div>
+            <button type="reset" class="btn btn-danger">Batal</button>
+            <button type="submit" class="btn btn-primary">Daftar</button>
+        </form>
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
