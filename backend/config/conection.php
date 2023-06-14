b2e35505d35dc3249766e9e6e3493d0512b8b28a
Ghautamaa <senjaghautama2004@gmail.com>
2023-06-14 09:19:27 +0800

    membuat template

---
+++ backend/config/conection.php
@@ -0,0 +1,16 @@
+<?php
+    $server     = 'localhost';
+    $user       = 'root';
+    $password   = '';
+    $database   = 'db_pendaftaran';
+
+    $db = mysqli_connect($server, $user, $password, $database);
+
+    if ($db) {
+    echo "berhasil";
+    }else {
+        die('gagal'.mysqli_connect_error());
+    }
+
+
+?>
