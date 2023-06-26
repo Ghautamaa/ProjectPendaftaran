ceb68398fb597dddf7a5ede2c465a4ff4e719b33
gunturdwiap <kadekguntur13@gmail.com>
2023-06-26 11:17:26 +0800

    proses-simpan-ml.php fix

--- resource/view/lomba-ml/proses-simpan-ml.php
+++ resource/view/lomba-ml/proses-simpan-ml.php
@@ -9,10 +9,12 @@ if (isset($_POST['submit'])) {
     $anggota3 = htmlspecialchars($_POST['anggota3']);
     $anggota4 = htmlspecialchars($_POST['anggota4']);
     $telp = htmlspecialchars($_POST['telp']);
+    $kategori_id = htmlspecialchars($_POST['kategori_id']);
+    $user_id = htmlspecialchars($_POST['user_id']);
 
 
-    $sql = "INSERT INTO tb_peserta (team, ketua, anggota1, anggota2, anggota3, anggota4, telp)
-            VALUES ('$tim', '$ketua', '$anggota1', '$anggota2', '$anggota3', '$anggota4', '$telp')";
+    $sql = "INSERT INTO tb_peserta (tim, ketua, anggota1, anggota2, anggota3, anggota4, telp, kategori_id, user_id)
+            VALUES ('$tim', '$ketua', '$anggota1', '$anggota2', '$anggota3', '$anggota4', '$telp', '$kategori_id','$user_id')";
     
     $query = mysqli_query($db, $sql);
     if ($query) {
