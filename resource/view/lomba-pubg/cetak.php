e82cc6bd1bb93805c9fd92aec153333e322e838f
Demonic <134345277+IKomangYoga@users.noreply.github.com>
2023-06-27 09:51:07 +0800

    update cetak

--- resource/view/lomba-pubg/cetak.php
+++ resource/view/lomba-pubg/cetak.php
@@ -4,32 +4,38 @@ require '../../../backend/fpdf.php';
 include '../../../backend/connection.php';
  
 // intance object dan memberikan pengaturan halaman PDF
-$pdf=new FPDF('P','mm','A4');
+$pdf=new FPDF('L','mm','A4');
 $pdf->AddPage();
  
-$pdf->SetFont('Times','B',13);
+$pdf->SetFont('Times','B',13,);
 
-$pdf->Cell(200,10,'DATA KARYAWAN',0,0,'C');
+$pdf->Cell(300,10,'DATA PESERTA LOMBA',0,0,'C');
  
 $pdf->Cell(10,15,'',0,1);
-$pdf->SetFont('Times','B',9);
-$pdf->Cell(10,7,'NO',1,0,'C');
-$pdf->Cell(50,7,'Tim' ,1,0,'C');
-$pdf->Cell(75,7,'Ketua',1,0,'C');
-$pdf->Cell(75,7,'Anggota1',1,0,'C');
-$pdf->Cell(75,7,'Anggota2',1,0,'C');
- 
+$pdf->SetFont('Times','B',11);
+$pdf->Cell(10,7,'No',1,0,'C');
+$pdf->Cell(40,7,'Tim' ,1,0,'C');
+$pdf->Cell(40,7,'Ketua',1,0,'C');
+$pdf->Cell(40,7,'Anggota1',1,0,'C');
+$pdf->Cell(40,7,'Anggota2',1,0,'C');
+$pdf->Cell(40,7,'Anggota3',1,0,'C');
+$pdf->Cell(40,7,'Anggota4',1,0,'C');
+$pdf->Cell(30,7,'Telepon',1,0,'C');
  
 $pdf->Cell(10,7,'',0,1);
-$pdf->SetFont('Times','',10);
-$no=1;
-$data = mysqli_query($db,"SELECT  * FROM tb_peserta");
-while($d = mysqli_fetch_array($data)){
-  $pdf->Cell(10,6, $no++,1,0,'C');
-  $pdf->Cell(50,6, $d['tim'],1,0);
-  $pdf->Cell(75,6, $d['ketua'],1,0);  
-  $pdf->Cell(55,6, $d['anggota1'],1,1);
-  $pdf->Cell(55,6, $d['anggota2'],1,1);
+
+$pdf->SetFont('Times', '', 10);
+$no = 1;
+$data = mysqli_query($db, "SELECT * FROM tb_peserta");
+while ($d = mysqli_fetch_array($data)) {
+  $pdf->Cell(10, 6, $no++, 1, 0, 'C');
+  $pdf->Cell(40, 6, $d['tim'], 1, 0, 'C');
+  $pdf->Cell(40, 6, $d['ketua'], 1, 0, 'C');
+  $pdf->Cell(40, 6, $d['anggota1'], 1, 0, 'C');
+  $pdf->Cell(40, 6, $d['anggota2'], 1, 0, 'C');
+  $pdf->Cell(40, 6, $d['anggota3'], 1, 0, 'C');
+  $pdf->Cell(40, 6, $d['anggota4'], 1, 0, 'C');
+  $pdf->Cell(30, 6, $d['telp'], 1, 1, 'C');
 }
  
 $pdf->Output();
