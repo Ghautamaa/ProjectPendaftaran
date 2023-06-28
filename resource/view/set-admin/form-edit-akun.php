6cfb35f54bf1a55b5635f57f950959905013b0bd
gunturdwiap <kadekguntur13@gmail.com>
2023-06-28 15:43:08 +0800

    Edit

--- resource/view/set-admin/form-edit-akun.php
+++ resource/view/set-admin/form-edit-akun.php
@@ -35,7 +35,7 @@ $akun = mysqli_fetch_array($query);
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-md-8 rounded-3 mb-2 text-white px-5" style="background-color: #0B132B;">
+            <div class="col-md-8 rounded-5 mb-2 text-white px-5" style="background-color: #0B132B;">
                 <form action="proses-edit-akun.php" method="post">
                     <input name="id" type="hidden" value="<?php echo $id; ?>">
                     <div class="mb-1 pt-3">
@@ -46,13 +46,12 @@ $akun = mysqli_fetch_array($query);
                         <label for="email" class="form-label ms-3 fw-bold">Email</label>
                         <input name="email" type="text" value="<?php echo $akun['email'] ?>" class="form-control rounded-pill" id="nama" placeholder="Email">
                     </div>   
-                    <div class="mb-1 mt-4">
-                        <label for="level" class="form-label ms-3 fw-bold">level</label>
-                        <select class="form-select" aria-label="Default select example" name="level" id="level" >
+                    <div class="mb-1">
+                        <label for="level" class="form-label ms-3 fw-bold">Level</label>
+                        <select class="form-select rounded-pill" aria-label="Default select example" name="level" id="level" >
                             <option value="admin">Admin</option>
                             <option value="peserta">Peserta</option>
                         </select>
-                        <!-- <input name="level" type="text" value="<?php echo $akun['level'] ?>" class="form-control rounded-pill" id="level" placeholder="level"> -->
                     </div>  
                     <div class="d-flex justify-content-end">
                         <button type="submit" name="submit" class="btn btn-lg btn-success my-5 px-5 rounded-4 shadow fw-bold">Edit</button> 
