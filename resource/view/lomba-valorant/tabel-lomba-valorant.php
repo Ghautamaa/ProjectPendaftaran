375f26a826f41ee7bb7a19e4a2059718eb12882c
MartabakEnak <arakianjuven@gmail.com>
2023-07-02 17:51:52 +0800

    valo

--- resource/view/lomba-valorant/tabel-lomba-valorant.php
+++ resource/view/lomba-valorant/tabel-lomba-valorant.php
@@ -105,7 +105,7 @@ include '../../../backend/connection.php'
                                 echo "<td>".$peserta['anggota4']."</td>";
                                 echo "<td>".$peserta['telp']."</td>";
                                
-                                echo "<td><a href='form-edit-valorant.php?id=".$peserta['id']."'><button class='btn btn-sm btn-warning text-white fw-bold'>Edit</button></a> |";
+                                echo "<td><a href='form-edit-valorant.php?id=".$peserta['id']."'><button class='btn btn-sm btn-warning text-white fw-bold'>Edit</button></a> | ";
                                 echo "    <a href='proses-hapus-valorant.php?id=".$peserta['id']."'><button class='btn btn-sm btn-danger text-white fw-bold' onclick=\"return confirm('Yakin?')\">Hapus</button></a></td>";
                                 echo "</tr>";
                                 $no++;
