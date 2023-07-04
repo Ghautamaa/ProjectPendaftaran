886c762de753ccc0ef7c1d2ef20d1f52b34d44f3
gunturdwiap <kadekguntur13@gmail.com>
2023-07-04 10:42:33 +0800

    update hapus mlbb

--- resource/view/lomba-ml/proses-hapus-ml.php
+++ resource/view/lomba-ml/proses-hapus-ml.php
@@ -1,14 +1,15 @@
 <?php
 include '../../../backend/connection.php';
 
-
-if (!isset($_GET['id']) || $_GET['id'] == null) {
+if (!isset($_GET['id'])) {
     header("Location: tabel-lomba-ml.php");
 }
 
 $id = $_GET['id'];
 
-$sql = "DELETE FROM tb_peserta WHERE id = $id";
+$sql = "DELETE 
+        FROM tb_mlbb 
+        WHERE id = $id";
 $query = mysqli_query($db, $sql);
 
 if ($query) {
