c327eb2e803fd2dcaab97d79c932b0d84805981f
MartabakEnak <arakianjuven@gmail.com>
2023-07-11 08:33:44 +0800

    Edit simpan pubg

--- resource/view/lomba-pubg/proses-simpan-pubg.php
+++ resource/view/lomba-pubg/proses-simpan-pubg.php
@@ -9,10 +9,9 @@ if (isset($_POST['submit'])) {
     $anggota2 = htmlspecialchars($_POST['anggota2']);
     $anggota3 = htmlspecialchars($_POST['anggota3']);
     $telp = htmlspecialchars($_POST['telp']);
-    $user_id = htmlspecialchars($_POST['user_id']);
 
-    $sql = "INSERT INTO tb_pubg (tim, ketua, anggota1, anggota2, anggota3, telp, user_id)
-            VALUES ('$tim', '$ketua', '$anggota1', '$anggota2', '$anggota3', '$telp', '$user_id')";
+    $sql = "INSERT INTO tb_pubg (tim, ketua, anggota1, anggota2, anggota3, telp)
+            VALUES ('$tim', '$ketua', '$anggota1', '$anggota2', '$anggota3', '$telp')";
     
     // Ngecek apakah berhasil ngirim apa kaga
     $query = mysqli_query($db, $sql);
